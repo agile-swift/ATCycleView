5be563aca50ec8554801bf947f1a2297b6cd4f74
马凯文 <admin@makaiwen.com>
2018-03-02 15:18:39 +0800

    feat : demo

--- Example/ATCycleView/ViewController.swift
+++ Example/ATCycleView/ViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import ATCycleView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ATCycleViewDelegate {
+    let colors = [UIColor.red,UIColor.blue,UIColor.yellow,UIColor.purple]
+    func numberOfItems(in cycleView: ATCycleView) -> Int {
+        return 4
+    }
+    
+    func cycleView(_ cycleView: ATCycleView, viewAtIndex index: Int, reuseView: UIView?) -> UIView {
+        var view : UIView?
+        if reuseView != nil {
+            view = reuseView
+        } else {
+            view = UIView.init(frame: cycleView.bounds)
+            view?.backgroundColor = colors[index]
+        }
+        
+        return view!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let v = ATCycleView.init(frame: CGRect.init(x: 0, y: 100, width: view.frame.width, height: 200))
+        v.delegate = self
+        v.pageMode = true
+        view.addSubview(v)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,5 +42,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
 }
 
